4f05148c635b0614aa1b6b6bb6879aef56da6935
Kila005 <maria.diaz7502@uco.net.co>
2025-09-28 22:20:33 -0500

    Mi primer commit

---
+++ ApiNose/src/main/java/co/edu/uco/nose/crosscuting/helper/ObjectHelper.java
@@ -0,0 +1,13 @@
+package co.edu.uco.nose.crosscuting.helper;
+
+public final class ObjectHelper {
+	private ObjectHelper() {
+	}
+	public static <O> boolean isNull(O object) {
+		return object == null;
+	}
+	public static <O> O getDefault(O object, O defaultValue) {
+		return isNull(object) ? defaultValue : object;
+	}
+	
+}
